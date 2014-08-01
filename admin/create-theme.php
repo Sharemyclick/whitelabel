c9e2af993cf67fd553f08a0ae38c7ef45bb6c6e7
Administrator <Administrator@192.168.1.83>
2014-08-01 16:56:08 +0200

    Merge origin/master
    Conflicts:
    	admin/conf.php
    	admin/view-users.php

---
+++ admin/create-theme.php
@@ -0,0 +1,250 @@
+<?php
+//It includes the page parameter connection.
+include('conf.php');
+
+?>
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Katniss Premium Admin Template</title>
+<link rel="stylesheet" href="css/style.default.css" type="text/css" />
+<link rel="stylesheet" href="prettify/prettify.css" type="text/css" />
+<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="js/jquery-migrate-1.1.1.min.js"></script>
+<script type="text/javascript" src="js/jquery-ui-1.9.2.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery.uniform.min.js"></script>
+<script type="text/javascript" src="prettify/prettify.js"></script>
+<script type="text/javascript" src="js/jquery.cookie.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+<script type="text/javascript">
+jQuery(document).ready(function (){
+	jQuery('[id=li-dashboard]').removeClass('active');
+	//jQuery('[id=li-view-coreg]').addClass('active');
+	jQuery('[id=li-users]').addClass('active');
+});
+</script>
+</head>
+
+<body>
+
+<div class="mainwrapper">
+	
+    <?php include ('./menu/menu-left.php');?>
+    
+    <!-- START OF RIGHT PANEL -->
+     <div class="rightpanel">
+    	<div class="headerpanel">
+        	<a href="" class="showmenu"></a>
+            
+            <div class="headerright">
+                
+                <div class="dropdown userinfo">
+                    <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="/page.html">Hi, <?php echo $_SESSION['login']; ?> <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="logout.php"><span class="icon-off"></span> Sign Out</a></li>
+                    </ul>
+                </div><!--dropdown-->
+    		
+            </div><!--headerright-->
+            
+    	</div><!--headerpanel-->
+        <div class="breadcrumbwidget">
+            <ul class="skins">
+                <li><a href="default" class="skin-color default"></a></li>
+                <li><a href="orange" class="skin-color orange"></a></li>
+		<li><a href="green" class="skin-color green"></a></li>
+                <li><a href="dark" class="skin-color dark"></a></li>
+                <li>&nbsp;</li>
+                <li class="fixed"><a href="" class="skin-layout fixed"></a></li>
+                <li class="wide"><a href="" class="skin-layout wide"></a></li>
+            </ul><!--skins-->
+            
+        </div><!--breadcrumbwidget-->
+        <div class="pagetitle">
+        	<h1>Create theme</h1> <span><?php echo $_SESSION['login']; ?> , Please fill in the form to create a new user.</span>
+        </div><!--pagetitle-->
+        
+        <div class="maincontent">
+		
+            <div class="contentinner">
+                
+                <?php
+                     if(isset($_POST['submit'])){
+                        
+//---------------------- INSERT THEMES ---------------------------------------------------------------------
+        $reqThemes = $bdd->prepare('INSERT INTO themes (name, image, picto, status, admin_id)'.
+        'VALUES (:name, :image, :picto, :status, :admin_id)');
+// We execute the request by transmitting the parameter list
+	$reqThemes->execute(array(
+		'name' => $_POST['name'],
+                'image' => $_POST['image'],
+                'picto' => $_POST['picto'],
+                'status' => $_POST['status'],
+                'admin_id' => $_POST['admin_id']
+                
+		)) or die(print_r($reqThemes->errorInfo())); // Oit tracks error if there is one
+                $id_themes = $bdd->lastInsertId();
+                // The request processing is terminated
+                $reqThemes->closeCursor();
+                         
+//---------------------- INSERT TEMPLATE_THEMES ---------------------------------------------------------------------
+        $reqThemesTemplate = $bdd->prepare('INSERT INTO template_themes (url, meta, title, text, alt, link, h1, themes_id)'.
+        'VALUES (:url, :meta, :title, :text, :alt, :link, :h1, :themes_id)');
+// We execute the request by transmitting the parameter list
+	$reqThemesTemplate->execute(array(
+		'url' => $_POST['url'],
+                'meta' => $_POST['meta'],
+                'title' => $_POST['title'],
+                'text' => $_POST['text'],
+                'alt' => $_POST['alt'],
+                'link' => $_POST['link'],
+                'h1' => $_POST['h1'],
+                'themes_id' => $id_themes
+		)) or die(print_r($reqThemesTemplate->errorInfo())); // Oit tracks error if there is one
+                // The request processing is terminated
+                $reqThemesTemplate->closeCursor();                    
+                         
+                         ?>   
+                
+                         <h4 class='confirmation' style="text-align: center; background:#1FC63D; opacity:0.8;">The theme has been created </h4> </br>
+                                    <p class="stdformbutton" style="text-align: center" >
+                                      <a href="create-theme.php" >
+                                        <button type="button" name="create_another_theme" id="create_another_theme" class="btn btn-primary" >Create another theme </button>
+                                      </a>
+                                     <a href="view-theme.php" >
+                                        <button type="button" name="view_all_theme" id="view_all_theme" class="btn btn-primary" >View all theme </button>
+                                      </a>
+                
+                                </p>   
+                
+                <?php ;}
+                Else {?>
+			<div class="widgetcontent">
+			
+            	<h4 class="widgettitle nomargin shadowed">Theme</h4>
+					
+                <div class="widgetcontent bordered shadowed nopadding">
+                    <form name="form_user" class="stdform stdform2" method="post" action="" enctype="multipart/form-data">
+                        
+                        <p>
+                            <label>Name *</label>
+                            <span class="field">
+                                <input type="text" name="name" class="input-xxlarge" required="required" />
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>Image *</label>
+                            <span class="field">
+                                <input type="text" name="image" class="input-xxlarge" required="required" />
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>Picto *</label>
+                            <span class="field">
+                                <input type="text" name="picto" class="input-xxlarge" required="required" />
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>Status *</label>
+                            <span class="field">
+                                <select name="status" id="status" class="status">
+                                        <option value="active"> Active</option>
+                                        <option value="non-active"> Non-active</option>
+                                </select>  
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>Admin *</label>
+                            <span class="field">
+                                <select id="admin_id" name="admin_id">
+                                <?php 
+                                $reqAdmin = $bdd->query('SELECT * FROM admin') or die(print_r($bdd->errorInfo()));
+
+                                while ($admin = $reqAdmin->fetch())
+                                    {?>
+                                    <option value="<?php echo $admin['id']; ?>"><?php echo $admin['company']; ?></option>
+                                <?php }?>
+                                </select>
+                            </span>
+                        </p>
+                        <h4 class="widgettitle nomargin shadowed">Template Theme</h4>
+                        
+                        <p>
+                            <label>URL *</label>
+                            <span class="field">
+                                <input type="url" name="url" class="input-xxlarge" required="required" />
+                            </span>
+                        </p>
+
+                        <p>
+                            <label>Meta  *</label>
+                            <span class="field">
+                                <input type="text" name="meta" class="input-xxlarge" required="required" />
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>Title  *</label>
+                            <span class="field">
+                                <input type="text" name="title" class="input-xxlarge" required="required" />
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>Text *</label>
+                            <span class="field">
+                                <input type="text" id="text"  name="text" class="input-xxlarge" required="required" />
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>Alt *</label>
+                            <span class="field">
+                                <input type="text" id="alt"  name="alt" class="input-xxlarge" required="required" />
+                            </span>
+                        </p>
+                        <p>
+                            <label>Link *</label>
+                            <span class="field">
+                                <input type="text" id="link"  name="link" class="input-xxlarge" required="required" />
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>h1 *</label>
+                            <span class="field">
+                                <input type="text" id="h1"  name="h1" class="input-xxlarge" required="required" />
+                            </span>
+                        </p>
+                                                                   
+                        <p class="stdformbutton" style="text-align: center">
+                            <button type="submit" name="submit" id="submit" class="btn btn-primary">Create </button>
+                            <button type="reset" class="btn">Reset </button>
+                        </p>
+                        
+                        </form>
+                    </div>				
+                </div><!--contentinner--> <?php }; ?>
+            </div><!--contentinner-->
+        </div><!--maincontent-->
+        
+    </div><!--rightpanel-->
+    <!-- END OF RIGHT PANEL -->
+    
+    <div class="clearfix"></div>
+    
+    <div class="footer">
+    	<div class="footerleft">Katniss Premium Admin Template v1.0</div>
+    	<div class="footerright">&copy; ThemePixels - <a href="http://twitter.com/themepixels">Follow me on Twitter</a> - <a href="http://dribbble.com/themepixels">Follow me on Dribbble</a></div>
+    </div><!--footer-->
+    
+</div><!--mainwrapper-->
+
+</body>
+</html>
---
+++ admin/view-theme-information.php
@@ -0,0 +1,203 @@
+<?php
+//It includes the page parameter connection.
+include('conf.php');
+$id_theme=$_GET['id'];
+?>
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Katniss Premium Admin Template</title>
+<link rel="stylesheet" href="css/style.default.css" type="text/css" />
+<link rel="stylesheet" href="prettify/prettify.css" type="text/css" />
+<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="js/jquery-migrate-1.1.1.min.js"></script>
+<script type="text/javascript" src="js/jquery-ui-1.9.2.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery.uniform.min.js"></script>
+<script type="text/javascript" src="prettify/prettify.js"></script>
+<script type="text/javascript" src="js/jquery.cookie.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+<script type="text/javascript">
+jQuery(document).ready(function (){
+	jQuery('[id=li-dashboard]').removeClass('active');
+	//jQuery('[id=li-view-coreg]').addClass('active');
+	jQuery('[id=li-users]').addClass('active');
+});
+</script>
+</head>
+
+<body>
+
+<div class="mainwrapper">
+	
+    <?php include ('./menu/menu-left.php');?>
+    
+    <!-- START OF RIGHT PANEL -->
+     <div class="rightpanel">
+    	<div class="headerpanel">
+        	<a href="" class="showmenu"></a>
+            
+            <div class="headerright">
+                
+                <div class="dropdown userinfo">
+                    <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="/page.html">Hi, <?php echo $_SESSION['login']; ?> <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="logout.php"><span class="icon-off"></span> Sign Out</a></li>
+                    </ul>
+                </div><!--dropdown-->
+    		
+            </div><!--headerright-->
+            
+    	</div><!--headerpanel-->
+        <div class="breadcrumbwidget">
+            <ul class="skins">
+                <li><a href="default" class="skin-color default"></a></li>
+                <li><a href="orange" class="skin-color orange"></a></li>
+		<li><a href="green" class="skin-color green"></a></li>
+                <li><a href="dark" class="skin-color dark"></a></li>
+                <li>&nbsp;</li>
+                <li class="fixed"><a href="" class="skin-layout fixed"></a></li>
+                <li class="wide"><a href="" class="skin-layout wide"></a></li>
+            </ul><!--skins-->
+            
+        </div><!--breadcrumbwidget-->
+        <div class="pagetitle">
+        	<h1>Create theme</h1> <span><?php echo $_SESSION['login']; ?> , Please fill in the form to create a new user.</span>
+        </div><!--pagetitle-->
+        
+        <div class="maincontent">
+		
+            <div class="contentinner">
+                
+               
+			<div class="widgetcontent">
+			
+            	<h4 class="widgettitle nomargin shadowed">Theme</h4>
+					
+                <div class="widgetcontent bordered shadowed nopadding">
+                    <form name="form_user" class="stdform stdform2" method="post" action="" enctype="multipart/form-data">
+                        <?php 
+                        $reqTheme = $bdd->query('SELECT * FROM themes LEFT JOIN template_themes ON template_themes.themes_id=themes.id WHERE themes.id='.$id_theme) or die(print_r($bdd->errorInfo())); // On traque l'erreur s'il y en a une
+					while ($theme = $reqTheme->fetch())
+						{?>
+                        
+                        <p>
+                            <label>Name *</label>
+                            <span class="field">
+                                <input type="text" name="name" class="input-xxlarge"  readonly="readonly" />
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>Image *</label>
+                            <span class="field">
+                                <input type="text" name="image" class="input-xxlarge" readonly="readonly" />
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>Picto *</label>
+                            <span class="field">
+                                <input type="text" name="picto" class="input-xxlarge" readonly="readonly"/>
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>Status *</label>
+                            <span class="field">
+                                <select name="status" id="status" class="status">
+                                        <option value="active"> Active</option>
+                                        <option value="non-active"> Non-active</option>
+                                </select>  
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>Admin *</label>
+                            <span class="field">
+                                <select id="admin_id" name="admin_id">
+                                <?php 
+                                $reqAdmin = $bdd->query('SELECT * FROM admin') or die(print_r($bdd->errorInfo()));
+
+                                while ($admin = $reqAdmin->fetch())
+                                    {?>
+                                    <option value="<?php echo $admin['id']; ?>"><?php echo $admin['company']; ?></option>
+                                <?php }?>
+                                </select>
+                            </span>
+                        </p>
+                        <h4 class="widgettitle nomargin shadowed">Template Theme</h4>
+                        
+                        <p>
+                            <label>URL *</label>
+                            <span class="field">
+                                <input type="url" name="url" class="input-xxlarge" required="required" />
+                            </span>
+                        </p>
+
+                        <p>
+                            <label>Meta  *</label>
+                            <span class="field">
+                                <input type="text" name="meta" class="input-xxlarge" required="required" />
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>Title  *</label>
+                            <span class="field">
+                                <input type="text" name="title" class="input-xxlarge" required="required" />
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>Text *</label>
+                            <span class="field">
+                                <input type="text" id="text"  name="text" class="input-xxlarge" required="required" />
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>Alt *</label>
+                            <span class="field">
+                                <input type="text" id="alt"  name="alt" class="input-xxlarge" required="required" />
+                            </span>
+                        </p>
+                        <p>
+                            <label>Link *</label>
+                            <span class="field">
+                                <input type="text" id="link"  name="link" class="input-xxlarge" required="required" />
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>h1 *</label>
+                            <span class="field">
+                                <input type="text" id="h1"  name="h1" class="input-xxlarge" required="required" />
+                            </span>
+                        </p>
+                                                                   
+                        <p class="stdformbutton" style="text-align: center">
+                            <button type="submit" name="submit" id="submit" class="btn btn-primary">Create </button>
+                            <button type="reset" class="btn">Reset </button>
+                        </p>
+                        
+                                                </form> <?php }?>
+                    </div>				
+                </div><!--contentinner--> 
+        </div><!--maincontent-->
+        
+    </div><!--rightpanel-->
+    <!-- END OF RIGHT PANEL -->
+    
+    <div class="clearfix"></div>
+    
+    <div class="footer">
+    	<div class="footerleft">Katniss Premium Admin Template v1.0</div>
+    	<div class="footerright">&copy; ThemePixels - <a href="http://twitter.com/themepixels">Follow me on Twitter</a> - <a href="http://dribbble.com/themepixels">Follow me on Dribbble</a></div>
+    </div><!--footer-->
+    
+</div><!--mainwrapper-->
+
+</body>
+</html>
