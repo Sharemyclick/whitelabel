dd0fd5eb531b90316c72248440702e511183737c
Administrator <Administrator@192.168.1.83>
2014-08-04 10:29:03 +0200

    Merge origin/master
    Conflicts:
    	admin/conf.php
    	admin/view-users.php

---
+++ admin/update-theme-globalview.php
@@ -0,0 +1,205 @@
+<?php
+// it includes parameters connection
+include('conf.php');
+
+    
+//Delete from table, in globalview page
+if(isset($_POST) && !empty($_POST)){
+    foreach($_POST as $indPost => $valPost){
+        if(strpos($indPost,'delete') !== false)
+        {
+            $bdd->exec('DELETE FROM themes WHERE id='.$_POST['id']);
+            $bdd->exec('DELETE FROM template_themes WHERE themes_id='.$_POST['id']); 
+        }
+    }
+}
+
+?>
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Katniss Premium Admin Template</title>
+<link rel="stylesheet" href="css/style.default.css" type="text/css" />
+<link rel="stylesheet" href="prettify/prettify.css" type="text/css" />
+<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="js/jquery-migrate-1.1.1.min.js"></script>
+<script type="text/javascript" src="js/jquery-ui-1.9.2.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery.uniform.min.js"></script>
+<script type="text/javascript" src="js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="prettify/prettify.js"></script>
+<script type="text/javascript" src="js/jquery.cookie.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+<script type="text/javascript">
+jQuery(document).ready(function (){
+	jQuery('[id=li-dashboard]').removeClass('active');
+	//jQuery('[id=li-view-coreg]').addClass('active');
+	jQuery('[id=li-users]').addClass('active');
+        
+        jQuery('[id^=delete]').click(function() {
+            var id = jQuery(this).attr('id').substring(6);
+            jQuery( "#form-delete-" + id ).submit();
+        });
+        
+});
+</script>
+</head>
+
+<body>
+    
+    <div class="mainwrapper">
+        
+        	<?php include ('./menu/menu-left.php');?>
+    
+    <!-- START OF RIGHT PANEL -->
+    <div class="rightpanel">
+    	<div class="headerpanel">
+        	<a href="" class="showmenu"></a>
+            
+            <div class="headerright">
+                
+    			<div class="dropdown userinfo">
+                    <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="/page.html">Hi, <?php echo $_SESSION['login']; ?> <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="logout.php"><span class="icon-off"></span> Sign Out</a></li>
+                    </ul>
+                </div><!--dropdown-->
+    		
+            </div><!--headerright-->
+            
+    	</div><!--headerpanel-->
+        <div class="breadcrumbwidget">
+        	<ul class="skins">
+                <li><a href="default" class="skin-color default"></a></li>
+                <li><a href="orange" class="skin-color orange"></a></li>
+				<li><a href="green" class="skin-color green"></a></li>
+                <li><a href="dark" class="skin-color dark"></a></li>
+                <li>&nbsp;</li>
+                <li class="fixed"><a href="" class="skin-layout fixed"></a></li>
+                <li class="wide"><a href="" class="skin-layout wide"></a></li>
+          
+        </div><!--breadcrumbwidget-->
+      <div class="pagetitle">
+        	<h1>Modify Theme</h1> <span><strong><?php echo ucfirst($_SESSION['login']); ?></strong> , please choose the theme to modify. </span>
+        </div><!--pagetitle-->
+        
+        <div class="maincontent">
+		
+        	<div class="contentinner content-dashboard">
+			
+            <table class="table table-bordered" id="dbase">
+                    <colgroup>
+                        <col class="con0" />
+                        <col class="con1" />
+                        <col class="con0" />
+                        <col class="con1" />
+                        <col class="con0" />
+                        <col class="con1" />
+                        <col class="con0" />
+                    </colgroup>
+ 
+                    <thead>
+                        <tr>
+                            <th class="centeralign">Name</th>
+                            <th class="centeralign">Image</th>
+                            <th class="centeralign">Picto</th>
+                            <th class="centeralign">Status </th>
+                            <th class="centeralign">Admin</th>
+                            <th class="centeralign">Update</th>
+                            <th class="centeralign">Delete</th>
+                    </thead>
+                    <tbody>
+					
+					<?php
+					// Request for data from questions
+					$reqTheme = $bdd->query('SELECT * FROM themes LEFT JOIN template_themes ON template_themes.themes_id=themes.id ORDER BY themes.name ASC') or die(print_r($bdd->errorInfo())); // On traque l'erreur s'il y en a une
+					while ($theme = $reqTheme->fetch())
+						{?>
+                        <tr><form class="stdform" method="post" action="update-theme.php?id=<?php echo $theme['id'];?>">
+
+                            <td class="centeralign" ><a href="update-theme.php?id=<?php echo $theme['id'];?>">
+                                                        <p align="center"> <?php echo $theme['name'] ?> </p></a>
+                            </td>
+                            
+                            <td>
+                                <p align="center"> <?php echo $theme['image'] ?> </p>
+                            </td>
+                            
+                            <td>
+                                <p align="center"> <?php echo $theme['picto'] ?> </p>
+                            </td>
+                            
+                            <td>
+                                 <p align="center"> <?php echo $theme['status'] ?> </p>
+                               
+                            </td>
+                            
+                            
+                            
+                            <td class="centeralign" style="text-align: left">
+                                 <p align="center">
+                                <?php 
+                                $reqAdmin = $bdd->query('SELECT * FROM admin WHERE id='.$theme['admin_id']) or die(print_r($bdd->errorInfo())); // it tracks error if there is one
+                                
+                                while ($admin= $reqAdmin->fetch())
+						{
+                                                    echo $admin['company'];
+                                                }
+                                ?></p>
+                            </td>
+                            <td class="centeralign"> <a href="update-theme.php?id=<?php echo $theme['id'] ?>" ><input type="button" class="btn btn-success" name="update" value="Update"> </a>
+                            </td>
+                          <!--'LEFT JOIN answers_questions ON answers_questions.questions_id=questions.id LEFT JOIN answers ON answers.id=answers_questions.answers_id '-->
+                      
+                        </form>
+                            <td class="centeralign" >
+                                <form  id="form-delete-<?php echo $theme['id']; ?>" name="form-delete-<?php echo $theme['id']; ?>" method="post" action="">
+                                  <a href="#"  class="deleterowcustomized"><span id="delete<?php echo $theme['id']; ?>" class="icon-trash"></span></a>
+                                  <input type="hidden" name="hidden_delete<?php echo $theme['id']; ?>" value="" >
+                                  <input type="hidden" name="id" value="<?php echo $theme['id']; ?>" >
+
+                                </form>
+                            </td>
+                            
+                       
+                        </tr>				
+                           <?php }
+					?>
+                    </tbody>
+				</table>
+				<script type="text/javascript">
+				// dynamic table
+				jQuery('#dbase').dataTable({
+				   "sPaginationType": "full_numbers",
+				   "aaSortingFixed": [[0,'asc']],
+				   "fnDrawCallback": function(oSettings) {
+					  jQuery.uniform.update();
+				   }
+				});
+				</script>
+				
+            </div><!--contentinner-->
+			
+        </div><!--maincontent-->
+        
+    </div><!--mainright-->
+    <!-- END OF RIGHT PANEL -->
+    
+    <div class="clearfix"></div>
+	
+    <div style="height:80px;"></div>
+	
+    <div class="footer">
+    	<div class="footerleft">Sharemyclick dashboard v1.0</div>
+    	<div class="footerright">&copy; Sharemyclick with Themepixels - <a href="https://twitter.com/sharemyclick"><span class="iconsweets-twitter"></a> - <a href="https://www.facebook.com/sharemyclick"><span class="iconsweets-facebook"></a> - <a href="https://www.linkedin.com/company/sharemyclick">Followus on Linkedin</a></div>
+    </div><!--footer-->
+ 
+</div><!--mainwrapper-->
+
+</body>
+</html>
+        
+        
+        
+        
---
+++ admin/update-theme.php
@@ -0,0 +1,230 @@
+<?php
+//It includes the page parameter connection.
+include('conf.php');
+$id_theme=$_GET['id'];
+?>
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Katniss Premium Admin Template</title>
+<link rel="stylesheet" href="css/style.default.css" type="text/css" />
+<link rel="stylesheet" href="prettify/prettify.css" type="text/css" />
+<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="js/jquery-migrate-1.1.1.min.js"></script>
+<script type="text/javascript" src="js/jquery-ui-1.9.2.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery.uniform.min.js"></script>
+<script type="text/javascript" src="prettify/prettify.js"></script>
+<script type="text/javascript" src="js/jquery.cookie.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+<script type="text/javascript">
+jQuery(document).ready(function (){
+	jQuery('[id=li-dashboard]').removeClass('active');
+	//jQuery('[id=li-view-coreg]').addClass('active');
+	jQuery('[id=li-users]').addClass('active');
+});
+</script>
+</head>
+
+<body>
+
+<div class="mainwrapper">
+	
+    <?php include ('./menu/menu-left.php');?>
+    
+    <!-- START OF RIGHT PANEL -->
+     <div class="rightpanel">
+    	<div class="headerpanel">
+        	<a href="" class="showmenu"></a>
+            
+            <div class="headerright">
+                
+                <div class="dropdown userinfo">
+                    <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="/page.html">Hi, <?php echo $_SESSION['login']; ?> <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="logout.php"><span class="icon-off"></span> Sign Out</a></li>
+                    </ul>
+                </div><!--dropdown-->
+    		
+            </div><!--headerright-->
+            
+    	</div><!--headerpanel-->
+        <div class="breadcrumbwidget">
+            <ul class="skins">
+                <li><a href="default" class="skin-color default"></a></li>
+                <li><a href="orange" class="skin-color orange"></a></li>
+		<li><a href="green" class="skin-color green"></a></li>
+                <li><a href="dark" class="skin-color dark"></a></li>
+                <li>&nbsp;</li>
+                <li class="fixed"><a href="" class="skin-layout fixed"></a></li>
+                <li class="wide"><a href="" class="skin-layout wide"></a></li>
+            </ul><!--skins-->
+            
+        </div><!--breadcrumbwidget-->
+        <div class="pagetitle">
+        	<h1>Update theme informations </h1> <span><?php echo $_SESSION['login']; ?> , Please see theme's information .</span>
+        </div><!--pagetitle-->
+        
+        <div class="maincontent">
+		
+            <div class="contentinner">
+                
+               
+			<div class="widgetcontent">
+			
+            	
+					
+                
+                    <?php if(isset($_POST['submit']))
+                    {
+//-----------------------------------------------------------------------------------// 
+//UPDATE themes table
+        $bdd->exec(' UPDATE themes SET name="'.$_POST['name'].'", image="'.$_POST['image'].'", picto="'.$_POST['picto'].'", status="'.$_POST['status'].'", admin_id="'.$_POST['admin_id'].'" WHERE id='.$id_theme);
+//---------------------------------------------------------------------------------------//
+        
+//-----------------------------------------------------------------------------------// 
+//UPDATE template_themes table
+        $bdd->exec(' UPDATE template_themes SET url="'.$_POST['url'].'", meta="'.$_POST['meta'].'", title="'.$_POST['title'].'", text="'.$_POST['text'].'", alt="'.$_POST['alt'].'", link="'.$_POST['link'].'", h1="'.$_POST['h1'].'" WHERE themes_id='.$id_theme);
+//---------------------------------------------------------------------------------------//
+                        
+                    ?>   
+                
+                         <h4 class='confirmation' style="text-align: center; background:#1FC63D; opacity:0.8;">The theme has been updated </h4> </br>
+                                    <p class="stdformbutton" style="text-align: center" >
+                                        <a href="update-theme-globalview.php" >
+                                        <button type="button" name="update_another_theme" id="update_another_quote" class="btn btn-primary" >Update another theme </button>
+                                      </a>
+                                        <a href="view-theme-globalview.php" >
+                                        <button type="button" name="view_all_theme" id="view_all_quote" class="btn btn-primary" >View all themes </button>
+                                      </a>
+                                </p>           
+                <?php }
+                    
+                    Else{?>
+                                <div class="widgetcontent bordered shadowed nopadding">
+                                <h4 class="widgettitle nomargin shadowed">Theme</h4>
+                    <form name="form_user" class="stdform stdform2" method="post" action="update-theme.php?id=<?php echo $id_theme; ?>" enctype="multipart/form-data">
+                        <?php 
+                        $reqTheme = $bdd->query('SELECT * FROM themes LEFT JOIN template_themes ON template_themes.themes_id=themes.id WHERE themes.id='.$id_theme) or die(print_r($bdd->errorInfo())); // On traque l'erreur s'il y en a une
+					while ($theme = $reqTheme->fetch())
+						{?>
+                        
+                        <p>
+                            <label>Name *</label>
+                            <span class="field">
+                                <input type="text" name="name" class="input-xxlarge" value="<?php echo $theme['name'] ?>" required="required" />
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>Image *</label>
+                            <span class="field">
+                                <input type="text" name="image" class="input-xxlarge" value="<?php echo $theme['image'] ?>" required="required"  />
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>Picto *</label>
+                            <span class="field">
+                                <input type="text" name="picto" class="input-xxlarge" value="<?php echo $theme['picto'] ?>" required="required" />
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>Status *</label>
+                            <span class="field">
+                                <select name="status" id="status" class="status" required="required" >
+                                        <option value="active" <?php if($theme['status']==='active'){echo 'selected';} ?> > Active</option>
+                                        <option value="non-active" <?php if($theme['status']==='non-active'){echo 'selected';} ?>> Non-active</option>
+                                </select>  
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>Admin *</label>
+                            <span class="field">
+                                <select id="admin_id" name="admin_id" required="required" >
+                                <?php 
+                                $reqAdmin = $bdd->query('SELECT * FROM admin') or die(print_r($bdd->errorInfo()));
+
+                                while ($admin = $reqAdmin->fetch())
+                                    {?>
+                                    <option value="<?php echo $admin['id']; ?>" <?php if($admin['id']===$theme['admin_id']){echo 'selected';} ?>>  <?php echo $admin['company']; ?></option>
+                                <?php }?>
+                                </select>
+                            </span>
+                        </p>
+                        <h4 class="widgettitle nomargin shadowed">Template Theme</h4>
+                        
+                        <p>
+                            <label>URL *</label>
+                            <span class="field">
+                                <input type="url" name="url" class="input-xxlarge" value="<?php echo $theme['url'] ?>" required="required"  />
+                            </span>
+                        </p>
+
+                        <p>
+                            <label>Meta  *</label>
+                            <span class="field">
+                                <input type="text" name="meta" class="input-xxlarge" value="<?php echo $theme['meta'] ?>" required="required"  />
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>Title  *</label>
+                            <span class="field">
+                                <input type="text" name="title" class="input-xxlarge" value="<?php echo $theme['title'] ?>" required="required" " />
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>Text *</label>
+                            <span class="field">
+                                <input type="text" id="text"  name="text" class="input-xxlarge" value="<?php echo $theme['text'] ?>" required="required"  />
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>Alt *</label>
+                            <span class="field">
+                                <input type="text" id="alt"  name="alt" class="input-xxlarge" value="<?php echo $theme['alt'] ?>" required="required"  />
+                            </span>
+                        </p>
+                        <p>
+                            <label>Link *</label>
+                            <span class="field">
+                                <input type="text" id="link"  name="link" class="input-xxlarge" value="<?php echo $theme['link'] ?>" required="required"  />
+                            </span>
+                        </p>
+                        
+                        <p>
+                            <label>h1 *</label>
+                            <span class="field">
+                                <input type="text" id="h1"  name="h1" class="input-xxlarge" value="<?php echo $theme['h1'] ?>" required="required"  />
+                            </span>
+                        </p>
+                                                                   
+                        <p class="stdformbutton" style="text-align: center">
+                            <button type="submit" name="submit" id="submit" class="btn btn-primary">Update </button>
+                        </p>
+                        
+                                                </form> <?php }}?>
+                    </div>				
+                </div><!--contentinner--> 
+        </div><!--maincontent-->
+        
+    </div><!--rightpanel-->
+    <!-- END OF RIGHT PANEL -->
+    
+    <div class="clearfix"></div>
+    
+    <div class="footer">
+    	<div class="footerleft">Katniss Premium Admin Template v1.0</div>
+    	<div class="footerright">&copy; ThemePixels - <a href="http://twitter.com/themepixels">Follow me on Twitter</a> - <a href="http://dribbble.com/themepixels">Follow me on Dribbble</a></div>
+    </div><!--footer-->
+    
+</div><!--mainwrapper-->
+
+</body>
+</html>
